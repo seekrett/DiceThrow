10bd4d124e1aeb68df40f7d4e44a139c073d3c8b
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-18 11:41:43 -0400

    fixed roll button

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+            // access function in DieFragment.kt from outside
+            // grab reference to that fragment
+            (supportFragmentManager
+                // returns a fragment instance           treated as a DieFragment.kt
+                .findFragmentById(R.id.fragmentContainerView) as DieFragment).throwDie()
+        }
     }
 }
