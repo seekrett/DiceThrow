1e8c00061486010b4365a05a552a1e1267ea1bb3
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-20 11:44:44 -0400

    added getter

--- app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
+++ app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
@@ -1,6 +1,20 @@
 package edu.temple.dicethrow
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DieViewModel : ViewModel() {
+    // val because data inside changes but livedata is the same
+    //      to observe data changes   v                 v delay instantiation; create only when needed
+    private val currentRoll : MutableLiveData<Int> by lazy {
+        //                                         ^ operations performed by something else
+        MutableLiveData()
+    }
+
+    // get approach            v don't want data to be changed
+    fun getCurrentRoll() : LiveData<Int> {
+        return currentRoll
+    }
+
 }
